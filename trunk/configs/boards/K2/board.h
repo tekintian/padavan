1a843bd0544b9f52bf66ff19a7ae3f9eef0c2f5a
tekintian <tekintian@gmail.com>
2025-11-28 15:32:50 +0800

    k2 test

---
+++ trunk/configs/boards/K2/board.h
@@ -0,0 +1,18 @@
+/* K2 */
+
+#define BOARD_PID		"K2"
+#define BOARD_NAME		"K2"
+#define BOARD_DESC		"PHICOMM K2 Wireless Router"
+#define BOARD_VENDOR_NAME	"PHICOMM"
+#define BOARD_VENDOR_URL	"http://dev.tekin.cn/"
+#define BOARD_MODEL_URL		"http://dev.tekin.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC		0  /* K2只有2.4G */
+#define BOARD_NUM_ANT_2G_TX		2
+#define BOARD_NUM_ANT_2G_RX		2
+#define BOARD_HAS_EPHY_L1000	0  /* K2使用百兆以太网 */
+#define BOARD_HAS_EPHY_W1000	0
+#define BOARD_NUM_ETH_LEDS		0
+#define BOARD_NUM_UPHY_USB3		0
