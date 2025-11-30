2598ec2b0506b04dc79c5f00656dc91a00185f9a
tekintian <tekintian@gmail.com>
2025-11-30 18:44:54 +0800

    tz.tz_minuteswest = (mktime(&gm) - mktime(&local)) / 60;

--- trunk/user/rc/src/rc.c
+++ trunk/user/rc/src/rc.c
@@ -583,10 +583,8 @@ setkernel_tz(void)
 	localtime_r(&now, &local);
 
 	gm.tm_isdst = local.tm_isdst;
-	// 时间问题处理
-	//tz.tz_minuteswest = (mktime(&gm) - mktime(&local)) / 60;
-	// 反向修正时区偏移计算，解决GMT+0显示CST时间的问题
-	tz.tz_minuteswest = -(mktime(&gm) - mktime(&local)) / 60;
+
+	tz.tz_minuteswest = (mktime(&gm) - mktime(&local)) / 60;
 
 	if (tz_minuteswest_last == tz.tz_minuteswest)
 		return;
