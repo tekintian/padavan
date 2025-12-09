13210d53c8c2e6fd42a695ca6a64b91637a8efad
tekintian <tekintian@gmail.com>
2025-12-09 22:15:38 +0800

    ADByBy 自定义过滤语法完整手册

--- trunk/user/adbyby/adbyby.sh
+++ trunk/user/adbyby/adbyby.sh
@@ -618,8 +618,7 @@ addscripts()
 	if [ ! -f "$adbyby_rules" ] || [ ! -s "$adbyby_rules" ] ; then
 	cat > "$adbyby_rules" <<-EOF
 !  ------------------------------ ADByby 自定义过滤语法简表---------------------------------
-!  --------------  规则基于abp规则，并进行了字符替换部分的扩展-----------------------------
-!  ABP规则请参考https://adblockplus.org/zh_CN/filters，下面为大致摘要
+! 完整手册请参考:https://gitee.com/tekintian/adt-rules/blob/master/adbyby/ADByBy_%E8%AF%AD%E6%B3%95%E6%89%8B%E5%86%8C.md，下面为大致摘要
 !  "!" 为行注释符，注释行以该符号起始作为一行注释语义，用于规则描述
 !  "*" 为字符通配符，能够匹配0长度或任意长度的字符串，该通配符不能与正则语法混用。
 !  "^" 为分隔符，可以是除了字母、数字或者 _ - . % 之外的任何字符。
